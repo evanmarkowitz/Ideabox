413379ae773f1681cfc53f361ca20e6a68874802
Evan Markowitz <emarkowitz1@gmail.com>
2019-04-04 15:21:40 -0600

    Enable Star True and False

--- idea.js
+++ idea.js
@@ -4,12 +4,12 @@ var saveButton = document.querySelector('.save-button');
 var quality = ["swill", "plausible", "genius"];
 
 class Idea {
-  constructor(id, title, body,) {
+  constructor(id, title, body, quality, starred) {
     this.id = id
     this.title = title;
     this.body = body;
-    this.quality = quality[0];
-    this.favorite = false;
+    this.quality = quality;
+    this.starred = starred;
   }
   upVote() {
     if(this.quality === quality[0]) {
@@ -25,12 +25,11 @@ class Idea {
     this.quality = quality[0]
     }
   }
-  starred() {
-    if (this.favorite === false) {
-      this.favorite = true;
-    }
-    if (this.favorite === true) {
-      this.favorite = false;
+  isStarred() {
+    if (this.starred === false) {
+      this.starred = true; }
+    else  {
+      this.starred = false;
     }
   }
   deleteFromStorage(suicide) {
