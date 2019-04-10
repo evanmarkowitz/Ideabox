26fc4d829b1d0cf14c837e1a90e376e6caca4545
Evan Markowitz <emarkowitz1@gmail.com>
2019-04-10 13:44:28 -0600

    Fix methods in idea.js

--- idea.js
+++ idea.js
@@ -2,8 +2,8 @@ var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
 var saveButton = document.querySelector('.save-button');
 var quality = ["swill", "plausible", "genius"];
-
 class Idea {
+
   constructor(id, title, body, quality, starred) {
     this.id = id
     this.title = title;
@@ -11,27 +11,28 @@ class Idea {
     this.quality = quality;
     this.starred = starred;
   }
-  upVote() {
-    var currentQualityIndex = quality.indexOf(this.quality)
-    if (currentQualityIndex < quality.length -1) {
-    var newQualityIndex = currentQualityIndex + 1
-    this.quality = quality[newQualityIndex]
-  }
-  }
-  downVote() {
-    var currentQualityIndex = quality.indexOf(this.quality)
-    if (currentQualityIndex > 0) {
-    var newQualityIndex = currentQualityIndex - 1;
-    this.quality = quality[newQualityIndex]
-    }
-  }
-  isStarred() {
-    if (this.starred === false) {
+
+  updateIdeaQuality(className) {
+    if (className === "quality-up-img"){
+      var currentQualityIndex = quality.indexOf(this.quality)
+      if (currentQualityIndex < quality.length -1) {
+      var newQualityIndex = currentQualityIndex + 1
+      this.quality = quality[newQualityIndex]
+    }}
+      else if (className === "quality-down-img") {
+      var currentQualityIndex = quality.indexOf(this.quality)
+      if (currentQualityIndex > 0) {
+      var newQualityIndex = currentQualityIndex - 1;
+      this.quality = quality[newQualityIndex]}}
+      else if (className === "fave-img") {
+      if (this.starred === false) {
       this.starred = true; }
-    else  {
+      else  {
       this.starred = false;
+    }}
     }
-  }
+
+
   deleteFromStorage(suicide) {
     return ideas.splice(suicide, 1);
   }
@@ -40,14 +41,47 @@ class Idea {
   var stringifiedIdeas = JSON.stringify(ideas);
   localStorage.setItem('ideas', stringifiedIdeas)
   }
-  editOfBody() {
-  if(e.target.className === "idea-card-body"){
-    var ideaLocation = findId(e);
-    ideas[ideaLocation].body = e.target.innerText
-    ideas[ideaLocation].saveToLocalStorage();
+  editIdeas(className, newText) {
+    
+    if (className === "idea-card-body"){
+    this.body = newText;}
+    else if (className === "idea-card-title"){
+    this.title = newText;
     }
+  }
 }
-}
+
+
+
+//   upVote() {
+//     var currentQualityIndex = quality.indexOf(this.quality)
+//     if (currentQualityIndex < quality.length -1) {
+//     var newQualityIndex = currentQualityIndex + 1
+//     this.quality = quality[newQualityIndex]
+//   }
+//   }
+//   downVote() {
+//     var currentQualityIndex = quality.indexOf(this.quality)
+//     if (currentQualityIndex > 0) {
+//     var newQualityIndex = currentQualityIndex - 1;
+//     this.quality = quality[newQualityIndex]
+//     }
+//   }
+//   isStarred() {
+//     if (this.starred === false) {
+//       this.starred = true; }
+//     else  {
+//       this.starred = false;
+//     }
+//   }
+//   editOfBody() {
+//   if(e.target.className === "idea-card-body"){
+//     var ideaLocation = findId(e);
+//     ideas[ideaLocation].body = e.target.innerText
+//     ideas[ideaLocation].saveToLocalStorage();
+//     }
+// }
+// }
  
 
 
