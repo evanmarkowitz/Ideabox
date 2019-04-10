8af3de1498f96b4b2d6579443841fcd88dfe4f63
Evan Markowitz <emarkowitz1@gmail.com>
2019-04-10 14:22:57 -0600

    Really Pretty Code

--- idea.js
+++ idea.js
@@ -55,32 +55,3 @@ class Idea {
 
 
 
-//   upVote() {
-//     var currentQualityIndex = quality.indexOf(this.quality)
-//     if (currentQualityIndex < quality.length -1) {
-//     var newQualityIndex = currentQualityIndex + 1
-//     this.quality = quality[newQualityIndex]
-//   }
-//   }
-//   downVote() {
-//     var currentQualityIndex = quality.indexOf(this.quality)
-//     if (currentQualityIndex > 0) {
-//     var newQualityIndex = currentQualityIndex - 1;
-//     this.quality = quality[newQualityIndex]
-//     }
-//   }
-//   isStarred() {
-//     if (this.starred === false) {
-//       this.starred = true; }
-//     else  {
-//       this.starred = false;
-//     }
-//   }
-//   editOfBody() {
-//   if(e.target.className === "idea-card-body"){
-//     var ideaLocation = findId(e);
-//     ideas[ideaLocation].body = e.target.innerText
-//     ideas[ideaLocation].saveToLocalStorage();
-//     }
-// }
-// }
